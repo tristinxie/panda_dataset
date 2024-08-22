a7378c1bfb2015565ec5a877b545d4c916236a49
Tristin Xie <tristinxie@gmail.com>
2024-08-21 22:00:02 -0700

    Generate gifs and publish joints and images in ros

--- README.md
+++ README.md
@@ -1,8 +1,33 @@
 # panda_dataset
 Dataset of Franka Emika Panda robot with ground truth camera-to-robot base, calibration parameters, joint angles, and video while it is teleoperated to do certain tasks.
 
+
 ## Download (7zip)
 - [10 episodes dataset](https://drive.google.com/file/d/1JN1RARqZaM1HpZEr-ZnHOR5Q0k_BtRLe/view?usp=drive_link)
 
+## 10 episodes visualized
+### Camera angle 1
+<img src="./assets/gifs/panda_0001.gif" height="120" width="160" />
+<img src="./assets/gifs/panda_0002.gif" height="120" width="160" />
+<img src="./assets/gifs/panda_0003.gif" height="120" width="160" />
+<img src="./assets/gifs/panda_0004.gif" height="120" width="160" />
+<img src="./assets/gifs/panda_0005.gif" height="120" width="160" />
+
+### Camera angle 2
+<img src="./assets/gifs/panda_0006.gif" height="120" width="160" />
+<img src="./assets/gifs/panda_0007.gif" height="120" width="160" />
+<img src="./assets/gifs/panda_0008.gif" height="120" width="160" />
+<img src="./assets/gifs/panda_0009.gif" height="120" width="160" />
+<img src="./assets/gifs/panda_0010.gif" height="120" width="160" />
+
 ## Usage
 `panda_dataset.py` contains the custom PyTorch dataloader. See `demo.ipynb` for more details.
+
+## Dataset Schema
+### Episode `info.json`
+```
+
+```
+### PyTorch Dataloader
+If you use the included PyTorch dataloader, we provide ready loaded data as additional keys you can access. You don't have to write code to read the files.
+
--- ros_publish.py
+++ ros_publish.py
@@ -5,7 +5,7 @@ from torch.utils.data import DataLoader
 
 import rospy
 from sensor_msgs.msg import JointState
-from std_msgs.msg import Header
+from sensor_msgs.msg import Image
 from rosgraph_msgs.msg import Clock
 
 if __name__ == "__main__":
@@ -14,11 +14,14 @@ if __name__ == "__main__":
     dataloader = DataLoader(panda_dataset, batch_size=1, num_workers=0, shuffle=True)
     sample = next(iter(dataloader))
 
-    pub = rospy.Publisher("/joint_states", JointState, queue_size=10)
+    joint_pub = rospy.Publisher("/joint_states", JointState, queue_size=10)
+    img_pub = rospy.Publisher("/rgb/image_raw", Image, queue_size=10)
     clk_pub = rospy.Publisher("/clock", Clock, queue_size=10)
+
     rospy.init_node("joint_state_publisher")
     rate = rospy.Rate(30)
     joint_msg = JointState()
+    image_msg = Image()
     clock = Clock()
     joint_msg.name = ["panda_joint1", "panda_joint2", "panda_joint3", "panda_joint4", "panda_joint5", "panda_joint6", "panda_joint7"]
     while not rospy.is_shutdown():
@@ -29,10 +32,20 @@ if __name__ == "__main__":
 
                 joint_msg.header.stamp = ros_timestamp
                 joint_msg.position = step["joint_angles"].squeeze().numpy()
+                joint_pub.publish(joint_msg)
 
-                pub.publish(joint_msg)
-                # clock.clock = ros_timestamp
-                # clk_pub.publish(clock)
+                clock.clock = ros_timestamp
+                clk_pub.publish(clock)
+
+                image_msg.header.stamp = ros_timestamp
+                image_msg.height = 240
+                image_msg.width = 320
+                image_msg.encoding = "rgb8"
+                image_msg.step = 240
+                image = list(step["img_data"].squeeze().permute(1,2,0).flatten().numpy())
+
+                image_msg.data = image
+                img_pub.publish(image_msg)
 
                 rate.sleep()
         except KeyboardInterrupt:
